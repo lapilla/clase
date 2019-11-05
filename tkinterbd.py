7a674f3bc1347f306914dac1ee6fd6bd69cbf83d
Lau <laura.castiblanco2@correo.usa.edu.co>
2019-11-05 12:42:28 -0500

    setup firebase

--- tkinterbd.py
+++ tkinterbd.py
@@ -1,3 +1,4 @@
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -11,7 +12,7 @@ import time
 cont=0
 prom=0
 
-placa = Arduino ('COM8')
+placa = Arduino ('COM3')
 it = util.Iterator(placa)
 it.start()
 a_0 = placa.get_pin('a:0:i')
@@ -21,32 +22,33 @@ led3 = placa.get_pin('d:6:p')
 led4 = placa.get_pin('d:9:p')
 led5 = placa.get_pin('d:10:p')
 led6 = placa.get_pin('d:11:p')
-time.sleep(0.5)
+time.sleep(0.1)
 ventana = Tk()
 ventana.geometry('1280x800')
 ventana.title("UI para sistemas de control")
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('keys/key.json')
+cred = credentials.Certificate('key/key.json')
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://bdtkinter.firebaseio.com/'
+    'databaseURL': 'https://clase-1ace7.firebaseio.com/'
 })
 
 
-marco1 = Frame(ventana, bg="gray", highlightthickness=1, width=1280, height=800, bd= 5)
+marco1 = Frame(ventana, bg="pink", highlightthickness=1, width=1280, height=800, bd= 5)
 marco1.place(x = 0,y = 0)
 b=Label(marco1,text="")
-img = Image.open("C:/Users/Camilo/Downloads/logousa.png")
+img = Image.open("C:/Users/Labing/Desktop/gatito1.png")
 img = img.resize((150,150), Image.ANTIALIAS)
 photoImg=  ImageTk.PhotoImage(img)
 b.configure(image=photoImg)
 b.place(x = 760,y = 20)
 
-valor= Label(marco1, bg='cadet blue1', font=("Arial Bold", 15), fg="white", width=5)
+valor= Label(marco1, bg='cadet blue', font=("Arial Bold", 15), fg="white", width=5)
 variable=StringVar()
-valor2= Label(marco1, bg='cadet blue1', font=("Arial Bold", 15), fg="white", width=5)
+valor2= Label(marco1, bg='cadet blue', font=("Arial Bold", 15), fg="white", width=5)
 adc_data=StringVar()
+
 
 def update_label():
     global cont
@@ -54,8 +56,9 @@ def update_label():
     ref = db.reference("sensor")
     ref.update({
                 'sensor1': {
-                    'adc': 0,
+                    'adc': cont +1,
                     'valor': cont,
+                    'temp' :100
                     
             }
          })
@@ -101,8 +104,6 @@ start_button2.place(x=80, y=160)
 
 save_button=Button(marco1,text="save",command=save)
 save_button.place(x=170, y=160)
-
-
 
 
 ventana.mainloop()
